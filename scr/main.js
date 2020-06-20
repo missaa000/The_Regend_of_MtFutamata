3ff33edfde40d8f606cc927f19eb2ec2b985e225
missaa000 <miyusaka1556@gmail.com>
2020-06-21 00:56:16 +0900

    added image rotate

--- scr/main.js
+++ scr/main.js
@@ -1,10 +1,12 @@
 //flags
 let gamestate;
-let point;
+let score;
 let level;
 let soba_i;
 let rock_i;
 let powerUp;
+let maxFlowing;
+let angle;
 
 //images
 let obj;
@@ -18,15 +20,17 @@ let nowImg;
 let titanx;
 let titany;
 let nowImgy;
+
+//Flowing items x coordenate
 let soba_arr = [800, 900, 1000, 800, 1200, 800, 900, 1000, 800, 1200, 800, 900, 1000, 800, 1200, 1000];
-let rock_arr = [1000, 900, 1000, 1300, 1500, 1000, 1400, 1200, 1000, 1300, 1000, 1400, 1200, 1000, 1300, 1000]
+let rock_arr = [1000, 900, 1000, 1300, 1500, 1000, 1400, 1200, 1000, 1300, 1000, 1400, 1200, 1000, 1300, 0]
 let s_titan = {
     x : 130,
     y : 290,
     img : 0,
     imgx : 120,
     imgy : 130,
-    p : 80,
+    score : 80,
 };
 
 let m_titan = {
@@ -35,7 +39,7 @@ let m_titan = {
     img : 0,
     imgx : 250,
     imgy : 300,
-    p : 120,
+    score : 120,
 };
 
 let l_titan = {
@@ -44,7 +48,7 @@ let l_titan = {
     img : 0,
     imgx : 350,
     imgy : 430,
-    p : 150,
+    score : 150,
 };
 
 let soba_para = {
@@ -60,7 +64,7 @@ let rock_para = {
     vy : 10
 };
 
-// load images
+// loading images
 function preload(){
     obj = loadImage('../image/objects.png');
     gnd = loadImage('../image/ground.png');
@@ -83,20 +87,22 @@ function setup(){
     
     negisoba = obj.get(30, 10, 130, 130);
     rock = obj.get(240, 30, 130, 110);
-    s_titan.img = obj.get(10, 370, 120, 130);
-    m_titan.img = obj.get(160, 180, 250, 300);
-    l_titan.img = obj.get(430, 40, 350, 430);
+    s_titan.img = obj.get(10, 370, s_titan.imgx, s_titan.imgy);
+    m_titan.img = obj.get(160, 180, m_titan.imgx, m_titan.imgy);
+    l_titan.img = obj.get(430, 40, l_titan.imgx, l_titan.imgy);
 
     nowImg = s_titan.img;
     titanx = s_titan.x;
     titany = s_titan.y;
     nowImgy = 0;
+    maxFlowing = 20;
     
     gamestate = 1;
-    point = 0;
+    score = 0;
     soba_i = 0;
     rock_i = 0;
     powerUp = true;
+    angle = 0;
 }
 
 
@@ -126,45 +132,40 @@ function draw(){
 
 //mainGame
 function mainGame(){
-    background(100, 150, 255);
-    image(gnd, 0, 0);
-    image(negisoba, soba_arr[soba_i], soba_para.y);
-    image(rock, rock_arr[rock_i], rock_para.y);
-
+    imgShow(0);
+    
     //titan size controll
     //small
-    if(point <= s_titan.p){
-	if(soba_i >= 15) endRun(5);
-	if(point == s_titan.p && powerUp){
+    if(score <= s_titan.score){
+	if(soba_i >= maxFlowing) endRun(5);
+	if(score == s_titan.score && powerUp){
 	    setData(m_titan.img, m_titan.imgy, m_titan.x, m_titan.y);
 	}
     }
 
     //mideum 
-    else if(point <= m_titan.p){
-	if(soba_i >= 15) endRun(4);
-	if(point < m_titan.p) powerUp = true;
-	if(point == m_titan.p && powerUp){
+    else if(score <= m_titan.score){
+	if(soba_i >= maxFlowing) endRun(4);
+	if(score < m_titan.score) powerUp = true;
+	if(score == m_titan.score && powerUp){
 	    setData(l_titan.img, l_titan.imgy, l_titan.x, l_titan.y);
 	}
     }
 
     //large
     else{
-	if(point >= l_titan.p) endRun(3);
-	else if(soba_i >= 15) endRun(4);
+	if(score >= l_titan.score) endRun(3);
+	else if(soba_i >= maxFlowing) endRun(4);
     }
-
-    image(nowImg, titanx, titany);
-
+  
     //increase point
     if(soba_arr[soba_i] <= 200 && soba_para.y <= (titany + nowImgy)){
-    	point += 10;
-    	console.log(point);
+    	score += 10;
+    	console.log(score);
     	soba_i++;
     }
 
-    //move objects
+    //moving objects
     soba_arr[soba_i] -= 10;
     rock_arr[rock_i] -= 10;
     if(soba_arr[soba_i] < -30) soba_i++;
@@ -174,7 +175,6 @@ function mainGame(){
     if(rock_arr[rock_i] == 200 && !keyIsPressed){
 	gamestate = 2;
     }
-
 }
 
 function setData(img, imgy, x, y){
@@ -185,8 +185,29 @@ function setData(img, imgy, x, y){
     powerUp = false;
 }
 
-function gameOver(){    
-  //  image(ending[3], 0, 0);
+function imgShow(flag){
+    background(100, 150, 255);
+    image(gnd, 0, 0);
+    image(negisoba, soba_arr[soba_i], soba_para.y);
+    image(rock, rock_arr[rock_i], rock_para.y);
+    translate(titanx, titany);
+    if(flag == 1){
+	rotate(radians(angle));
+	angle += 4;
+    }
+    image(nowImg, 0, 0);
+}
+
+function gameOver(){
+    titanx += 20;
+    titany -= 10;
+    imgShow(1);
+
+    if(titany < -500){
+	//image(ending[3], 0, 0);
+	titany = -500;
+	background(100, 200, 300);
+    }
 }
 
 function endRun(state){
@@ -209,7 +230,6 @@ function badEnd(){
 }
 
 function keyPressed() {
-    console.log(keyCode);
     if (keyCode === UP_ARROW) {
 	titany -= 150;
     }
@@ -218,3 +238,11 @@ function keyPressed() {
 function keyReleased() {
     titany += 150;
 }
+
+// function mousePressed(){
+//     titany -= 150;
+// }
+
+// function mouseReleased(){
+//     titany += 150;
+// }
